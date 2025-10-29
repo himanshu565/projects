27b803ccdee05727746449e15e3e8ed46f3cb046
l3002 <tanmaymathpal4545@gmail.com>
2025-10-30 03:49:17 +0530

    chore: removed unused data and event types
    Signed-off-by: l3002 <tanmaymathpal4545@gmail.com>

--- src/types/dataTypes.ts
+++ src/types/dataTypes.ts
@@ -53,25 +53,6 @@ interface TeamPageDetails extends TeamCardDetails {
     }[],
 }
 
-export class FileChunk{
-    static readonly MAX_CHUNK_SIZE: number = 4096;
-    static readonly DEFUALT_INIT_CHUNK_SIZE: number = 1024;
-    chunkVer: number; // 4Kib of data
-    chunkBegin: number;
-    chunkSize: number = FileChunk.DEFUALT_INIT_CHUNK_SIZE;
-
-    constructor(chunkVer: number, chunkBegin: number);
-    constructor(chunkVer: number, chunkBegin: number, chunkSize: number);
-
-    constructor(chunkVer: number, chunkBegin: number, chunkSize?: number){
-        this.chunkVer = chunkVer;
-        this.chunkBegin = chunkBegin;
-        if(chunkSize !== undefined) {
-            this.chunkSize = chunkSize;
-        }
-    }
-}
-
 export type { InviteResponse };
 export type { OAuthTokenResponse };
 export type { TeamCardDetails };
--- src/types/eventType.ts
+++ src/types/eventType.ts
@@ -3,8 +3,6 @@ import type { FileChunk } from "./dataTypes.js";
 // interface for Server to Client Events
 export interface ServerToClientEvents{
     fileData: (chunk: string) => void; //sends file in chunks
-    chunkDetails: (fileChunks: FileChunk[]) => void;
-    chunkChanged: (chunkId: number, chunk: string, chunkVer: string) => void;
     userLeft: (userId: string) => void;
     userOpenedFile: (userId: string) => void;
     userWriting: (userId: string, caretPos: number) => void;
@@ -14,7 +12,6 @@ export interface ServerToClientEvents{
 // interface for Client to Server Events
 export interface ClientToServerEvents{
     getFileData: (fileId: string) => void;
-    chunkChanged: (chunkId: number, chunk: string, vChunkId: number) => void;
     write: (caretPos: number) => void;
 }
 
